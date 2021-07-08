078c92f1515ccba4d4d27fe3cd054a4cc9ac62ee
Mvestrotech <contact@ngarnier.fr>
2021-07-08 20:35:17 +0200

    add creatPlayer

--- src/main/java/fr/esgi/timebomb/controller/PlayerController.java
+++ src/main/java/fr/esgi/timebomb/controller/PlayerController.java
@@ -1,14 +1,17 @@
 package fr.esgi.timebomb.controller;
 
 import fr.esgi.timebomb.domain.Player;
+import fr.esgi.timebomb.exceptions.PlayerEmptyException;
 import fr.esgi.timebomb.repository.PlayerRepository;
+import fr.esgi.timebomb.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -17,10 +20,34 @@ import static org.springframework.http.ResponseEntity.ok;
 public class PlayerController {
 
     @Autowired
-    private PlayerRepository playerDao;
+    private PlayerService playerService;
 
     @GetMapping
-    public ResponseEntity<List<Player>> listPlayer(){
-        return ok(playerDao.findAll());
+    public Iterable<Player> getPlayers() {
+        return playerService.getPlayers();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createPlayer(@RequestBody Player player) throws PlayerEmptyException {
+        if (player.getUsername() == null) {
+            throw new PlayerEmptyException("Player Value is not empty");
+        }
+        Player created = playerService.savePlayer(player);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    @GetMapping("/{id}")
+    public Player getPlayer(@PathVariable("id") final Long id) {
+        Optional<Player> player = playerService.getPlayer(id);
+        if(player.isPresent()) {
+            return player.get();
+        } else {
+            return null;
+        }
     }
 }
